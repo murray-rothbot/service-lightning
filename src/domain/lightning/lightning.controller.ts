d343090d51b17336c249493eeed7a14f567bdbf2
Miguel Medeiros <miguel@miguelmedeiros.com.br>
2024-02-04 21:33:55 -0300

    fix: change swagger setup

--- src/domain/lightning/lightning.controller.ts
+++ src/domain/lightning/lightning.controller.ts
@@ -1,20 +1,24 @@
 import { Controller, Get, Param } from '@nestjs/common'
 import { LightningService } from './lightning.service'
+import { ApiTags } from '@nestjs/swagger'
 
 @Controller('')
 export class LightningController {
   constructor(private readonly lightningService: LightningService) {}
 
+  @ApiTags('Lightning')
   @Get('/statistics')
   async getStatistics() {
     return this.lightningService.getNetworkStatistics()
   }
 
+  @ApiTags('Lightning')
   @Get('/top')
   async getTopNodes() {
     return this.lightningService.getTopNodes()
   }
 
+  @ApiTags('Lightning')
   @Get('/node/:pubKey')
   async getNode(@Param() params) {
     return this.lightningService.getNode({ ...params })
